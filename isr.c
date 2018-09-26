45ffc2812a5ea34422710799f5fd790dd63b968d
KatyaNiosi <plimmi.1@gmail.com>
2018-09-25 18:11:47 -0700

    start of phase2

--- isr.c
+++ isr.c
@@ -47,3 +47,35 @@ void TimerISR(void) {
    return;
 }
 
+void GetPidISR(void){
+  pcb[cur_pid].TF_p->ebx = cur_pid;
+}
+
+void SleepISR(void){
+  // get sleep second from ebx of the TF of process cur_pid
+  // set the wake time in PCB of process cur_pid to cur OS time + sleep second * 100
+  // change the state of process cur_pid
+  // reset cur_pid to ...
+  pcb[cur_pid].wake_time = sys_ticks + 100 * (pcb[cur_pid].TF_p->ebx);
+  pcb[cur_pid].state = SLEEPY;
+  cur_pid = -1;
+}
+
+void SetVideoISR(void){
+  //get row from ebx in TF of process cur_pid
+  //get col from ecx in TF of process cur_pid
+  //set video pointer(video_p) to: HOME_POS + (row-1) * 80 + (col-1)
+  int row = pcb[cur_pid].TF_p->ebx;
+  int col = pcb[cur_pid].TF_p->ecx;
+  *video_p = HOME_POS + (row-1) * 80 + (col-1);
+}
+
+void WriteISR(void){
+  int device = pcb[cur_pid].TF_p->ebx;
+  char *str = pcb[cur_pid].TF_p->ecx;
+
+  if(device == STDOUT) {
+
+  }
+}
+
