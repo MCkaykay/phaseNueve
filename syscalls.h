a2d1efe284c4e487c528bac440e72e1baa1f8739
KatyaNiosi <plimmi.1@gmail.com>
2018-09-26 12:44:04 -0700

    start of phase 2

---
+++ syscalls.h
@@ -0,0 +1,13 @@
+// syscalls.h, 159
+
+#ifndef __lib_H__
+#define __lib_H__
+
+#include "types.h"
+
+void Sleep(int);
+int GetPId(void);
+void SetVideo(int, int);
+void Write(int, char *);
+
+#endif
